81f7d02e50a5acdc169306175d69412bc24bc9d2
AdamusPL <adam.czekalski.szczecin@gmail.com>
2024-05-11 13:32:08 +0200

    test

--- Lab3/lab3/vite.config.js
+++ Lab3/lab3/vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: 'public/index.html', // default
+      },
+    },
+  },
   server: {
     open: "/public/index.html"
   }
